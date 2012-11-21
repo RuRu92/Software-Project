28d9199e4ac0866740f8e07ead0478f896992017
Ru Ru <Ru_Ru3000@hotmail.co.uk>
2012-11-21 23:06:13 +0000

    New classes

---
+++ Project/CompRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class CompRoom : RoomCL
+    {
+
+        private int m_type;
+
+        //Constructor with inherited propertied from Events class
+        public CompRoom(int roomID, String rName, int capacity) : base(roomID, rName, capacity)
+        {
+            m_type = type;
+        }
+
+        public int type
+        {
+            get
+            {
+                return m_type;
+            }
+
+            set
+            {
+                m_type = value;
+            }
+        }
+    }
+}
---
+++ Project/NetRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class NetRoom : RoomCL
+    {
+        private int m_type;
+
+        //Constructor with inherited propertied from Events class
+        public NetRoom(int roomID, String rName, int capacity)  : base(roomID, rName, capacity)
+        {
+            m_type = type;
+        }
+
+        public int type
+        {
+            get
+            {
+                return m_type;
+            }
+
+            set
+            {
+                m_type = value;
+            }
+        }
+    }
+}
