08664a96baac0b8cfc534c15c6617659376d7707
unknown <rjonusas@HP-012.l410.cwa.ac.uk>
2012-11-22 08:49:36 +0000

    Rooooooooom....

---
+++ Project/LabRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class LabRoom : RoomCL
+    {
+        String m_type;
+
+        //Constructor with inherited propertied from Events class
+        public LabRoom(int roomID, String rName, int capacity, String Type)
+            : base(roomID, rName, capacity)
+        {
+            m_type = type;
+        }
+
+        public String type
+        {
+            get
+            {
+                return m_type;
+            }
+            set
+            {
+                m_type = value;
+            }
+        }
+    }
+}
---
+++ Project/RoomCSuite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class RoomCSuite : RoomCL
+    {
+        String m_type;
+
+        //Constructor with inherited propertied from Events class
+        public RoomCSuite(int roomID, String rName, int capacity, String Type)
+            : base(roomID, rName, capacity)
+        {
+            m_type = type;
+        }
+
+        public String type
+        {
+            get
+            {
+                return m_type;
+            }
+            set
+            {
+                m_type = value;
+            }
+        }
+    }
+}
